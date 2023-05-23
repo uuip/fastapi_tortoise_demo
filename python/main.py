6beafd293367f6fca5c1b3a71db7c69b68b901f1
uuip <uuip@users.noreply.github.com>
2023-05-23 19:17:52 +0800

    set port to 8000

--- python/main.py
+++ python/main.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8003,
+        port=8000,
         reload=False,
         workers=os.cpu_count(),
         loop="uvloop",
