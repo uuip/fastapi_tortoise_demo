b7db3fac16275bca5ac55ccb900c473bf6b05143
uuip <uuip@users.noreply.github.com>
2023-07-23 03:04:20 +0800

    update

--- python/db.py
+++
@@ -1,5 +0,0 @@
-from urllib.parse import urlparse
-
-from settings import settings
-
-url = urlparse(settings.db).geturl()
--- python/settings.py
+++ python/settings.py
@@ -1,13 +1,7 @@
 from pathlib import Path
-from typing import Optional
-from urllib.parse import urlparse
 
-from pydantic import Field, model_validator
+from pydantic import Field
 from pydantic_settings import SettingsConfigDict, BaseSettings
-
-# from pydantic import Json,RedisDsn,HttpUrl,EmailStr
-# from ipaddress import IPv4Address
-# from pathlib import Path
 
 _env_file = Path(__file__).parent / ".env"
 
@@ -16,29 +10,6 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=_env_file, extra="ignore")
 
     db: str = Field(alias="db")
-    db_dict: Optional[dict]
-    db_django: Optional[dict]
-
-    @model_validator(mode="before")
-    def set_variant(cls, values: dict):
-        c = urlparse(values["db"])
-        values["db_dict"] = {
-            "host": c.hostname,
-            "port": c.port or 5432,
-            "database": c.path.lstrip("/"),
-            "user": c.username,
-            "password": c.password,
-        }
-
-        values["db_django"] = {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": c.path.lstrip("/"),
-            "USER": c.username,
-            "PASSWORD": c.password,
-            "HOST": c.hostname,
-            "PORT": c.port or 5432,
-        }
-        return values
 
 
 settings = Settings()
